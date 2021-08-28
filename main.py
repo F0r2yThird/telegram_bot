4a36b36130754176e1446d4ffe88658bdd36c7e4
Алексей  Сухов <aleksejsuhov@MacBook-Pro-Aleksej.local>
2021-08-29 02:14:07 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import telebot
+bot = telebot.TeleBot('1031196043:AAH1g4GzAfsR-7PebjLGuplNd3L1Sk9Eqz4')
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    bot.send_message(message.chat.id, "Привет, давай начнем знакомиться!)")
+bot.polling()
